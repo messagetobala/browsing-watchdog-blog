2b7c7968c8606512983de2d26c1a3fd6acaa35e5
Balakrishnan Rajkumar <messagetobala@gmail.com>
2023-01-07 17:50:25 +0530

    Fixed styling for MVP1

--- chrome/mvp1/content-script.js
+++ chrome/mvp1/content-script.js
@@ -2,12 +2,13 @@ function modifyContent() {
     document.body.textContent = ""
 
     let bodyDiv = document.createElement('div')
-    bodyDiv.setAttribute("id", "body")
+    bodyDiv.setAttribute("class", "watchdog_body")
 
     let headerSection = document.createElement('section')
-    headerSection.setAttribute("id", "header")
+    headerSection.setAttribute("class", "watchdog_header_section")
 
     let headerPara_1 = document.createElement('p')
+    headerPara_1.setAttribute("class", "watchdog_para")
     headerPara_1.textContent = 'A slight change in your daily habits can guide your life to a very different destination.'
 
     let headerPara_2 = document.createElement('p')
