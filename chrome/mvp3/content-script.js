a91960da61bfb56c1340e16eef6a412e8cce4e28
Balakrishnan Rajkumar <messagetobala@gmail.com>
2023-01-07 18:12:31 +0530

    Fixed MVP3

--- chrome/mvp3/content-script.js
+++ chrome/mvp3/content-script.js
@@ -2,17 +2,19 @@ function modifyContent(request, sender, sendResponse) {
     document.body.textContent = ""
 
     let bodyDiv = document.createElement('div')
-    bodyDiv.setAttribute("id", "body");
-    
+    bodyDiv.setAttribute("class", "watchdog_body")
+
     let headerSection = document.createElement('section')
-    headerSection.setAttribute("id", "header")
-    
+    headerSection.setAttribute("class", "watchdog_header_section")
+
     let headerPara_1 = document.createElement('p')
+    headerPara_1.setAttribute("class", "watchdog_para")
     headerPara_1.textContent = 'A slight change in your daily habits can guide your life to a very different destination.'
-    
+
     let headerPara_2 = document.createElement('p')
+    headerPara_2.setAttribute("class", "watchdog_para")
     headerPara_2.textContent = '- James Clear'
-    
+
     headerSection.appendChild(headerPara_1)
     headerSection.appendChild(headerPara_2)
 
